5968227ecb0e94a8dea9aafab68e0a18673b959f
Julian Hamelberg <jshx@mit.edu>
2020-06-01 16:59:20 -0400

    move webserver into worker.js

--- app/worker.js
+++ app/worker.js
@@ -11,6 +11,28 @@ class Worker extends SCWorker {
     var environment = this.options.environment;
 
     var app = express();
+    app.use(serveStatic(path.resolve(__dirname, 'public')));
+    app.use(express.urlencoded());
+    app.use(express.json());
+
+    app.get('/operator', function(req, res){
+        res.sendFile(path.join(__dirname + '/public/password.html'));
+      });
+      
+    app.post('/controller', function(req, res){
+        if (req.body.password == 'lagpolice') {
+        res.sendFile(path.join(__dirname + '/public/operator.html'));
+        } else {
+            return res.status(401).send();
+        }
+    });
+    
+    app.post('/ua', function(nav){
+        console.log(nav.body);
+    });
+
+    
+
     if (environment === 'dev') {
       // Log every HTTP request.
       // See https://github.com/expressjs/morgan for other available formats.
@@ -37,8 +59,11 @@ class Worker extends SCWorker {
       });
 
     });
+    let port = 3000;
+    app.listen(port, console.log("Starting Server at port " + port));
 
   }
+  
 }
 
 new Worker();
